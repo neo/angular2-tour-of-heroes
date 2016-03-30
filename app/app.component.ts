2e4c5bb524c19c486a2a8be19aaad94b89e4c2cc
Neo <neolwc@gmail.com>
2016-03-29 23:57:57 -0400

    Services (1/2)

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,7 @@
 import {Component} from 'angular2/core';
 import {Hero} from './hero';
 import {HeroDetailComponent} from './hero-detail.component';
+import {HeroService} from './hero.service';
 
 @Component({
 	selector: 'my-app',
@@ -65,24 +66,19 @@ import {HeroDetailComponent} from './hero-detail.component';
 			border-radius: 4px 0 0 4px;
 		}
 	`],
-	directives: [HeroDetailComponent]
+	directives: [HeroDetailComponent],
+	providers: [HeroService]
 })
 export class AppComponent {
 	title = 'Tour of Heros';
 	selectedHero: Hero;
-	heroes = HEROES;
+	heroes: Hero[];
+
+	constructor(private _heroService: HeroService) {}
+
+	getHeroes() {
+		this.heroes = this._heroService.getHeroes();
+	}
+
 	onSelect(hero: Hero) { this.selectedHero = hero; }
 }
-
-var HEROES: Hero[] = [
-	{ "id": 11, "name": "Mr. Nice" },
-	{ "id": 12, "name": "Narco" },
-	{ "id": 13, "name": "Bombasto" },
-	{ "id": 14, "name": "Celeritas" },
-	{ "id": 15, "name": "Magneta" },
-	{ "id": 16, "name": "RubberMan" },
-	{ "id": 17, "name": "Dynama" },
-	{ "id": 18, "name": "Dr IQ" },
-	{ "id": 19, "name": "Magma" },
-	{ "id": 20, "name": "Tornado" }
-];
